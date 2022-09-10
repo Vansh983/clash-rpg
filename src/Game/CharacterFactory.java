914137bc63866cf5c56e2f49a91e1d53fc2aa386
Vansh Sood <emailofvansh@gmail.com>
2022-09-10 14:58:23 -0300

    setup

---
+++ src/Game/CharacterFactory.java
@@ -0,0 +1,15 @@
+package Game;
+import java.awt.*;
+
+public class CharacterFactory {
+
+    public static RPGCharacter getCharacter(){
+
+        return null;
+    }
+
+    public static String getPossibleCharacters(){
+        return "Mage, Priest, Warrior";
+    }
+
+}
